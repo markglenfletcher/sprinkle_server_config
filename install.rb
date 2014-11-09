b4d35fd87944b3d7a1603cdb3f2eb3484a2f98b5
Mark Glen Fletcher <markglenfletcher@googlemail.com>
2014-11-09 23:35:09 +0000

    Remove this line, dir is created in previous step

--- install.rb
+++ install.rb
@@ -13,12 +13,11 @@ deployment do
 
   package :digicert_root_ca do
     description 'Install digicert root ca cert for SSL comms with github via wget'
-    runner 'mkdir /usr/local/share/ca-certificates'
     runner 'wget -cq -O /usr/local/share/ca-certificates/DigiCertHighAssuranceEVRootCA.crt http://cacerts.digicert.com/DigiCertHighAssuranceEVRootCA.crt'
     runner 'update-ca-certificates'
     requires :ca_certificates
   end
-
+  
   policy :sprinkle_server, roles: :app do
     requires :build_essential
     requires :ca_certificates
